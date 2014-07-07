550b30f8f899e7a6e44100cd31f0ceb201762cd5
Tarun Bhardwaj <tarun.bhardwaj@openlabs.co.in>
2014-07-07 13:39:18 +0530

    Added documentation compatible to ngDocs

--- src/tryton.js
+++ src/tryton.js
@@ -1,10 +1,3 @@
-/**
-@fileOverview
-
-@toc
-
-*/
-
 'use strict';
 
 angular.module('openlabs.angular-tryton', ['ngStorage'])
@@ -18,17 +11,134 @@ angular.module('openlabs.angular-tryton', ['ngStorage'])
         // The __error__ attribute is set by the response Transformer
         var error = angular.copy(response.data);
         if (error[0] == 'NotLogged') {
+          /**
+           * @ngdoc event
+           * @name tryton:NotLogged
+           * @eventOf openlabs.angular-tryton.service:tryton
+           * @eventType broadcast on root scope
+           *
+           * @description
+           *
+           *  **tryton:NotLogged**
+           *
+           *  Raised when the current session token is expired or invalid.
+           *  Depending on the application, it could decide to logoff the user
+           *  or show a lock screen and ask for the password again to refresh
+           *  the login by calling `session.doLogin`.
+           *
+           *  Example:
+           *
+           *  ```js
+           *  $rootScope.$on('tryton:NotLogged', function(){
+           *    // do something
+           *  })
+           *  ```
+           *
+           */
           $rootScope.$broadcast('tryton:NotLogged');
         } else if (error[0] == 'UserError') {
+          /**
+           * @ngdoc event
+           * @name tryton:UserError
+           * @eventOf openlabs.angular-tryton.service:tryton
+           * @eventType broadcast on root scope
+           *
+           * @description
+           *
+           *  **tryton:UserError**
+           *
+           *  Raised when an error occurs on processing the request and tryton
+           *  handled it. UserErrors are usually known exception like invalid
+           *  data or not meeting the requirements.
+           *
+           *  Example:
+           *
+           *  ```js
+           *  $rootScope.$on('tryton:UserError', function(message, description){
+           *    // do something
+           *  })
+           *  ```
+           *
+           */
           $rootScope.$broadcast('tryton:UserError', error[1], error[2]);
         } else if (error[0] == 'UserWarning') {
+          /**
+           * @ngdoc event
+           * @name tryton:UserWarning
+           * @eventOf openlabs.angular-tryton.service:tryton
+           * @eventType broadcast on root scope
+           *
+           * @description
+           *
+           *  **tryton:UserWarning**
+           *
+           *  Raised when a warning message needs to be showed to the user.
+           *  The `name` can be used to suppress this warning from being shown
+           *  in the future. This should be handled by the application logic
+           *  and ng-tryton will not handle it.
+           *
+           *  Example:
+           *
+           *  ```js
+           *  $rootScope.$on('tryton:UserWarning', function(name, message, description){
+           *    // do something
+           *  })
+           *  ```
+           *
+           */
           $rootScope.$broadcast('tryton:UserWarning', error[1], error[2], error[3]);
         } else if (error[0] == 'ConcurrencyException') {
+          /**
+           * @ngdoc event
+           * @name tryton:ConcurrencyException
+           * @eventOf openlabs.angular-tryton.service:tryton
+           * @eventType broadcast on root scope
+           *
+           * @description
+           *
+           *  **tryton:ConcurrencyException**
+           *
+           *  Exception raised when there is a concurrency exception reported
+           *  by the optimistic lock. Your application depending on how complex
+           *  you want it to be could decide to handle this and ask the user
+           *  to compare or confirm the overwrite. This exception is usually
+           *  seen when some other user has already updated the record after it
+           *  was loaded by the current user (based on timestamp).
+           *
+           *  Example:
+           *
+           *  ```js
+           *  $rootScope.$on('tryton:ConcurrencyException', function(error){
+           *    // do something
+           *  })
+           *  ```
+           *
+           */
           $rootScope.$broadcast('tryton:ConcurrencyException', error[1]);
         } else {
-          // An exception that was not one of the above types. Usually an
-          // error model and a stack trace are returned. the returned error
-          // array is passed as such to the broadcast.
+          /**
+           * @ngdoc event
+           * @name tryton:Exception
+           * @eventOf openlabs.angular-tryton.service:tryton
+           * @eventType broadcast on root scope
+           *
+           * @description
+           *
+           *  **tryton:Exception**
+           *
+           *  An exception that was not one of the handled types. Usually an
+           *  error model and a stack trace are returned. which is passed as
+           *  such in the broadcast.
+           *
+           *  Example:
+           *
+           *  ```js
+           *  $rootScope.$on('tryton:Exception', function(error){
+           *    // do something
+           *  })
+           *  ```
+           *
+           */
           $rootScope.$broadcast('tryton:Exception', error);
         }
         // Finally reject the promise with the response itself
@@ -62,9 +172,36 @@ angular.module('openlabs.angular-tryton', ['ngStorage'])
   };
   $httpProvider.defaults.transformResponse.push(trytonResponseTransformer);
 }])
+
+
+/**
+ * @ngdoc service
+ * @name openlabs.angular-tryton.service:tryton
+ *
+ * @description
+ * Low level service to invoke RPC on a tryton server.
+ *
+ * While this method exposes the implementation of the RPC client, this is
+ * required only when you have to call a common service (eg. getting server
+ * version) or authenticating. The `rpc` method takes parameters which should
+ * include the user ID and session.
+ *
+ * The `session` service provides a higher level abstraction which offers an
+ * RPC client that automatically handles authentication and context for you.
+ *
+ */
 .service('tryton', ['$http', function ($http) {
   var tryton = this;
 
+  /**
+    @ngdoc property
+    @name serverUrl
+    @propertyOf openlabs.angular-tryton.service:tryton
+
+    @description
+      Stores the URL of the RPC server to which the connection has to be
+      made.
+  **/
   this.serverUrl = '/';
 
   // Change this URL using setServerUrl
@@ -73,9 +210,57 @@ angular.module('openlabs.angular-tryton', ['ngStorage'])
     tryton.serverUrl = url;
   };
 
+  /**
+    @ngdoc method
+    @name rpc
+    @methodOf openlabs.angular-tryton.service:tryton
 
-  // The lowest level RPC calling methanism which calls the given
-  // database url over http with the method and parameters.
+    @description
+      The lowest level RPC client which invokes the `method` with the
+      provided `params` and optionally a database.
+
+    @param {string} method An RPC method that need to be called.
+    @param {array} params Parameters to be passed to the `method` including the
+                          user ID and session key. If you are looking for this
+                          to be automatically handled, look at `session.rpc`
+                          instead.
+    @param {string} [database=""] Tryton database on which this request need to
+                                  be processed. Leave empty for calls that do
+                                  not need a database (Ex. getVersion).
+    @returns {Promise}  Promise that will be resolved when the success response
+                        come from server.
+
+    @example
+      <example module="example">
+        <file name="index.html">
+         <div ng-controller="TrytonCtrl">
+           <span ng-init="getServerVersion()">
+             Server Version: {{ serverVersion }}
+           </span>
+         </div>
+        </file>
+        <file name="app.js">
+          angular.module('example', [
+            'openlabs.angular-tryton'
+          ])
+          .controller('TrytonCtrl', [
+            '$scope',
+            'tryton',
+            function ($scope, tryton) {
+              tryton.serverUrl = '/';
+              $scope.getServerVersion = function () {
+                // Call Tryton JsonRPC server and return promise.
+                tryton.rpc('common.version', [null, null])
+                  .success(function (result) {
+                    // Do something with result
+                    $scope.serverVersion = result;
+                  });
+              }
+            }
+          ]);
+        </file>
+      </example>
+  **/
   this.rpc = function(method, params, database) {
     var request = $http.post(
       tryton.serverUrl + (database || ''),
@@ -87,28 +272,113 @@ angular.module('openlabs.angular-tryton', ['ngStorage'])
     return request;
   };
 
-  // Get the server's version by calling rpc method
+  /**
+    @ngdoc method
+    @name getServerVersion
+    @methodOf openlabs.angular-tryton.service:tryton
+
+    @description
+      Convenience wrapper to fetch server version.
+
+    @returns {Promise} Promise that will be resolved on successful response.
+
+    @example
+      <example module="example">
+        <file name="index.html">
+         <div ng-controller="TrytonCtrl">
+           <span ng-init="getServerVersion()">
+             Server Version: {{ serverVersion }}
+           </span>
+         </div>
+        </file>
+        <file name="app.js">
+           angular.module('example', [
+             'openlabs.angular-tryton'
+           ])
+           .controller('TrytonCtrl', [
+             '$scope',
+             'tryton',
+             function ($scope, tryton) {
+               tryton.serverUrl = '/';
+               $scope.getServerVersion = function () {
+                 // Call Tryton JsonRPC server and return promise.
+                 tryton.getServerVersion()
+                   .success(function (result) {
+                     // Do something with result
+                     $scope.serverVersion = result;
+                   });
+               }
+             }
+           ]);
+        </file>
+      </example>
+  **/
   this.getServerVersion = function() {
     return tryton.rpc('common.version', [null, null]);
   };
 
 }])
+
+/**
+ * @ngdoc service
+ * @name openlabs.angular-tryton.service:session
+ *
+ * @description
+ * High level service to invoke `tryton` service.
+ *
+ * While this method exposes the implementation of the RPC client, this is
+ * required only when you have to call a common service (eg. getting server
+ * version) or authenticating. The `rpc` method takes parameters which should
+ * include the user ID and session.
+ *
+ * The `session` service provides a higher level abstraction which offers an
+ * RPC client that automatically handles authentication and context for you.
+ *
+ */
 .service('session', ['tryton', '$localStorage', '$sessionStorage', function(tryton, $localStorage, $sessionStorage) {
   // Controller for managing tryton session
 
   var session = this;
 
-  // The Integer ID of the currently logged in User
+  /**
+    @ngdoc property
+    @name userId
+    @propertyOf openlabs.angular-tryton.service:session
+
+    @description
+      The Integer ID of the currently logged in User
+  **/
   this.userId = null;
 
-  // The Session ID of the current session (issued by tryton server)
+  /**
+    @ngdoc property
+    @name sessionId
+    @propertyOf openlabs.angular-tryton.service:session
+
+    @description
+      The Session ID of the current session (issued by tryton server)
+  **/
   this.sessionId = null;
 
-  // The database to which this session connects to
+  /**
+    @ngdoc property
+    @name database
+    @propertyOf openlabs.angular-tryton.service:session
+
+    @description
+      The database to which this session connects to
+  **/
   this.database = null;
 
-  // The login/username used to connect to tryton. This has to be remembered
-  // for just asking for the password when a session times out.
+  /**
+    @ngdoc property
+    @name login
+    @propertyOf openlabs.angular-tryton.service:session
+
+    @description
+      The login/username used to connect to tryton. This has to be remembered
+      for just asking for the password when a session times out.
+  **/
   this.login = null;
 
   // The context object of the session. This is the default context from the
@@ -140,10 +410,27 @@ angular.module('openlabs.angular-tryton', ['ngStorage'])
     delete $sessionStorage.context;
   };
 
+  /**
+    @ngdoc method
+    @name setSession
+    @methodOf openlabs.angular-tryton.service:session
+
+    @description
+      Set the user and session ID and also save them to localStorage for
+      retrieval. Set null if value is undefined; happens when user attempts to
+      log in to incompatible database.
+
+      ***Note:** This method need not be explicitly called as `session.doLogin`
+      automatically calls this method on successful login.
+
+    @param {string} [database=null] Tryton database on which this request need
+                                  to be processed.
+    @param {string} login The login/username used to connect to tryton.
+    @param {integer} userId The UserId of currently logged in user.
+    @param {string} sessionId The Session ID of the current session (issued by
+                              tryton server).
+  **/
   this.setSession = function(_database, _login, _userId, _sessionId) {
-    // Set the user and session ID and also save them to cookie store for
-    // retreival. Set null if value is undefined; happens when user attempts to
-    // log in to incompatible database.
     $localStorage.database = _database || null;
     $localStorage.login = _login || null;
     $localStorage.userId = _userId || null;
@@ -154,6 +441,51 @@ angular.module('openlabs.angular-tryton', ['ngStorage'])
     loadAllFromCookies();
   };
 
+
+  /**
+    @ngdoc method
+    @name rpc
+    @methodOf openlabs.angular-tryton.service:session
+
+    @description
+      Make remote procedure call on the given `method` with the `params` and
+      optional `context`. This is a convenience wrapper over `tryton.rpc`. It
+      automatically constructs the parameters as required by `tryton.rpc` (with
+      userID, session and the context).
+
+      Example:
+
+      ```js
+      session.rpc(
+        'party.party.read',   // method
+        [[1, 2], ['name']],   // params: ids, field_names
+        {company: 1}          // context
+      )
+      ```
+
+      Equivalent Python:
+
+      ```python
+      with Transaction().set_context(company=1):
+        Party.read([1, 2], ['name'])
+      ```
+
+    @param {string} method Name of the RPC method that need to be called
+                           (Ex. `'party.party.read'`).
+    @param {array} params Parameters to be passed to the method.
+                          Unlike, `tryton.rpc` you do not have to pass the
+                          userID, session or the context. They are automatically
+                          constructed, before being passed to `tryton.rpc`.
+                          (Ex. `[[1, 2], ['name']]`)
+    @param {Object} context A javascript object with any additional context that
+                            needs to be set. The context is merged with the
+                            default context in the server. The default context
+                            is the context returned by the get_preferences
+                            method.
+                            Note: JavaScript object should be JSON serializable.
+                            (Ex: `{company: 1}`)
+
+  **/
   this.rpc = function(_method, _params, _context) {
     // Make a remote procedure call to tryton with the current user ID
     // session and the database in the session
@@ -170,6 +502,16 @@ angular.module('openlabs.angular-tryton', ['ngStorage'])
     return tryton.rpc(_method, params, session.database);
   };
 
+  /**
+    @ngdoc method
+    @name doLogout
+    @methodOf openlabs.angular-tryton.service:session
+
+    @description
+      Log the current user out and clear out the session.
+
+    @returns {Promise} Promise that will be resolved on successful response.
+  **/
   this.doLogout = function() {
     var promise = session.rpc('common.db.logout');
     // Clean logout of the user
@@ -182,6 +524,19 @@ angular.module('openlabs.angular-tryton', ['ngStorage'])
     loadAllFromCookies();
   };
 
+  /**
+    @ngdoc method
+    @name doLogin
+    @methodOf openlabs.angular-tryton.service:session
+
+    @description
+      Login to the given `database` using `username` and `password`.
+      If the login is successful, the method calls `session.setSession` and
+      saves the session token which can be used for subsequent requests made
+      through `session.rpc`.
+
+    @returns {Promise} Promise that will be resolved on successful response.
+  **/
   this.doLogin = function(_database, _username, _password) {
     // call login on tryton server and if the login is succesful set the
     // userId and session
@@ -197,11 +552,52 @@ angular.module('openlabs.angular-tryton', ['ngStorage'])
     return promise;
   };
 
+  /**
+    @ngdoc method
+    @name isLoggedIn
+    @methodOf openlabs.angular-tryton.service:session
+
+    @description
+      Check if the user is logged in.
+
+      ***Note:** This method does not guarantee that the session is still valid.
+      It only ensures that the user logged in once.
+
+    @returns {bool} `true` or `false`
+  **/
   this.isLoggedIn = function () {
     return !!session.sessionId;
   }
 
 }])
+
+
+  /**
+    @ngdoc filter
+    @name openlabs.angular-tryton.service:urlTryton
+
+    @description
+      Create a tryton scheme URL which can be opened with the URL handlers of
+      the operating system. This is useful if you want to cross link to records
+      or windows that you need to open on the tryton client from your web
+      application.
+
+      Example:
+
+      ```html
+      <a href="{{ 'party.party'|urlTryton:party.id }}">
+       Click here to open Party:{{ party.name }} in your tryton client
+      </a>
+      ```
+
+    @param {string} name Name of the resource (model/wizard/report).
+                         Ex: `party.party`
+    @param {integer} [id=null] Optional id of the record. Leaving this empty
+                               would trigger a list view on tryton
+    @param {string} [type='model'] Type of the resource identified by name. It
+                                   could be `'model'`, `'wizard'` or `'report'`.
+
+  **/
 .filter('urlTryton', function ($window, tryton, session) {
   return function (name, id, type_) {
     type_ = type_ || 'model';
